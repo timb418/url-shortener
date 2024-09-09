9433711bb4367a87fc28b47c254af504313fc8ec
Timur <10380147+timb418@users.noreply.github.com>
2024-09-09 15:26:00 +0300

    feat: iter1 impl, initial handler logic

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/timb418/url-shortener/internal/app/handlers"
+	"github.com/timb418/url-shortener/internal/app/server"
+)
+
+func main() {
+	srv := server.NewServer()
+	handlers.RegisterRoutes(srv)
+	log.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", srv); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/timb418/url-shortener
+
+go 1.22.5
